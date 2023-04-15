6cf1c6fd02e3bbb8c83717486b93dfa59f295bfa
Daniel Hernández <dant.hdez02@gmail.com>
2023-04-14 23:41:02 -0600

    Final Layout

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.forms[0];
+const addTask = document.getElementById('delete');
+const inputTask = document.getElementById('input-task');
+//const task = document.getElementById
+
+
+
