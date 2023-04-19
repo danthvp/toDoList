03e0a8cf0339b5b5a57b0facfa3b3aa15d79ad8d
Daniel Hernández <dant.hdez02@gmail.com>
2023-04-19 17:04:11 -0600

    New layout and add task functionality

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const form = document.forms[0];
-const addTask = document.getElementById('delete');
+const addTaskButton = document.getElementById('create-task');
 const inputTask = document.getElementById('input-task');
-//const task = document.getElementById
+const list = document.getElementById('task-list');
+const deleteButton = document.querySelectorAll('delete');
+
+addTaskButton.addEventListener('click', (e)=>{
+    let listElement = document.createElement("li");
+    let task = document.createTextNode(inputTask.value);
+    let deleteTask = document.createElement("button");
+    let buttonText = document.createTextNode('X');
+    deleteTask.classList.add('delete');
+    deleteTask.appendChild(buttonText);
+    listElement.appendChild(task);
+    listElement.appendChild(deleteTask);
+    list.appendChild(listElement);
+    
+    console.log(deleteButton);
+    inputTask.value = "";
+})
+
 
 
 
