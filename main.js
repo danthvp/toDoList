7b30a4c64de3b746a765833816b8c2c714c6f590
Daniel Hernández <dant.hdez02@gmail.com>
2023-04-20 15:59:29 -0600

    Finished functionalities

--- main.js
+++ main.js
@@ -2,23 +2,31 @@ const form = document.forms[0];
 const addTaskButton = document.getElementById('create-task');
 const inputTask = document.getElementById('input-task');
 const list = document.getElementById('task-list');
-const deleteButton = document.querySelectorAll('delete');
+
 
 addTaskButton.addEventListener('click', (e)=>{
-    let listElement = document.createElement("li");
-    let task = document.createTextNode(inputTask.value);
-    let deleteTask = document.createElement("button");
-    let buttonText = document.createTextNode('X');
-    deleteTask.classList.add('delete');
-    deleteTask.appendChild(buttonText);
-    listElement.appendChild(task);
-    listElement.appendChild(deleteTask);
-    list.appendChild(listElement);
-    
-    console.log(deleteButton);
-    inputTask.value = "";
+    if(inputTask.value != "") {
+        let listElement = document.createElement("li");
+        let task = document.createTextNode(inputTask.value);
+        listElement.appendChild(task);
+        listElement.appendChild(addDeleteButton());
+        list.appendChild(listElement);
+        
+        inputTask.value = "";
+    }
 })
 
 
 
+function addDeleteButton() {
+    let deleteTask = document.createElement("button");
+    deleteTask.textContent = "X";
+    deleteTask.className = "delete-btn";
 
+    deleteTask.addEventListener('click', (e)=> {
+        const item = e.target.parentElement;
+        list.removeChild(item);
+    })
+    return deleteTask;
+
+}
